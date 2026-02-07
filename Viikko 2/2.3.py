eae586f35b8dd55e243ecc3d2bb3453aa676f103
henry <henry.klavus@metropolia.fi>
2026-02-07 13:43:04 +0200

    Viikko 2

--- Viikko 2/2.3.py
+++ Viikko 2/2.3.py
@@ -2,7 +2,8 @@ import math
 
 kanta = int(input("Syötä suorakulmion kanta: "))
 korkeus = int(input("Syotä suorakulmion korkeus: "))
-
+print("Pinta-ala: ")
 A = print(kanta * korkeus)
+print("Piiri: ")
 Piiri = print(2 * kanta + 2 * korkeus)
 
---
+++ Viikko 2/2.4.py
@@ -0,0 +1,7 @@
+Ensimmäinen = int(input("Syötä luku 1: "))
+Toinen = int(input("syötä luku 2: "))
+Kolmas = int(input("Syötä luku 3: "))
+
+print(Ensimmäinen + Toinen + Kolmas)
+print(Ensimmäinen * Toinen * Kolmas)
+print((Ensimmäinen + Toinen + Kolmas) / 3)
