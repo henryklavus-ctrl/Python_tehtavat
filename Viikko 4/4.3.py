945b59eb81d2ce7589c10490430dc27b32ee97b2
henry <henry.klavus@metropolia.fi>
2026-03-02 13:06:30 +0200

    Viikko 4

---
+++ Viikko 4/4.3.py
@@ -0,0 +1,12 @@
+while True:
+    luku = str(input("Anna luku: "))
+    tyhja = ""
+    if luku == tyhja:
+        print("Ohjelma lopetetaan")
+        print("Suurin luku: ")
+        print("Pienin luku: ")
+        break
+
+    
+
+
