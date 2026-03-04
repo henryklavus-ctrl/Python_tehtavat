2b7040e6aa1364a8ede831f2345f1303d4eda0db
henry <henry.klavus@metropolia.fi>
2026-03-04 17:27:45 +0200

    Viikko 4

---
+++ Kertaus/1.py
@@ -0,0 +1,7 @@
+nimi = input("Anna nimesi: ")
+if nimi == "Matti":
+    print("Seuraava, kiitos!")
+else :
+    keittoannos = int(input("Montako annosta?: "))
+    hinta = keittoannos * 5.90
+    print(hinta)
