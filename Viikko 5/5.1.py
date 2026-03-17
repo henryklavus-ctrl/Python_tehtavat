c83135737e4162a401fce9076e1c8f385e41343b
henry <henry.klavus@metropolia.fi>
2026-03-17 13:42:12 +0200

    Viikko 4

---
+++ Viikko 5/5.1.py
@@ -0,0 +1,11 @@
+import random
+nopat = int(input("Anna arpakuutioiden määrä: "))
+
+summa = 0
+
+for n in range(nopat):
+    heitot = random.randint(1,6)
+    summa += heitot
+
+print(f"Noppien summa on {summa}")
+
